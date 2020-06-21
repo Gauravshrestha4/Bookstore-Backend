0ba2d20fb6d03f040e94a404a62a18483602a670
Abhinav Sharma <sharma.abhinav503@gmail.com>
2020-06-21 10:44:46 -0400

    Adding return type to function definition

--- src/bookstore.ts
+++ src/bookstore.ts
@@ -1,7 +1,7 @@
 export class Bookstore {
     static version = '1'
     constructor() {}
-    add(a: number, b: number) {
+    add(a: number, b: number):number {
         console.log(`version ${Bookstore.version}`)
         return a + b
     }
